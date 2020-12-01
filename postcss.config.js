698df7b260ecac4bdc77a873cf4d8bd48f78f798
Arwaz Jamal <arwaz.jamal@codinova.com>
2020-12-01 10:29:02 +0530

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('postcss-apply'),
+        require('postcss-import'),
+        require('postcss-nested'),
+        require('postcss-nesting')
+    ]
+}
